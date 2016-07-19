2b0c28676ebe858f7e1489e027a3f524e6bdc948
Raul F. de Melo <melo.raulf@gmail.com>
2016-07-18 23:17:33 -0300

    Criado o formulario e preparada a rotina para inserção do ponto

--- app/routes/ponto.js
+++ app/routes/ponto.js
@@ -1,19 +1,35 @@
 module.exports = function(app) {
     app.get('/ponto', function(req, res) {
-
-
         var connection = app.infra.connectionFactory();
-        var produtosDAO = new app.infra.produtosDAO(connection); //o new cria um novo contexto
+        var pontoDAO = new app.infra.pontoDAO(connection); //o new cria um novo contexto
 
-        produtosDAO.lista(function(erro, resultados) {
+        pontoDAO.lista(function(erro, resultados) {
 
             res.render('inicial', {
                 lista: resultados
             });
-
         });
-
         connection.end();
+    });
 
-    })
+    app.get('/ponto/new', function(req, res) {
+
+        res.render('cadastra-ponto');
+    });
+
+    app.post('/ponto', function(req, res) {
+        var ponto = req.body; //pega corpo da requisição
+
+        var connection = app.infra.connectionFactory();
+        var pontoDAO = new app.infra.pontoDAO(connection); //o new cria um novo contexto
+
+        pontoDAO.insere(ponto, function(erro, resultado) {
+            if (!erro) {
+                res.redirect('/ponto'); //always redirect after post!
+            } else {
+                res.send(erro);
+            }
+        });
+        connection.end();
+    });
 }
