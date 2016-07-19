78eb594e76392a9210fd5a1f02113fb7b6314cf0
Raul F. de Melo <melo.raulf@gmail.com>
2016-07-18 23:18:08 -0300

    Adicionado o modulo BodyParser para tratar os form nas requisições

--- config/express.js
+++ config/express.js
@@ -1,16 +1,23 @@
 var express = require('express')();
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
-
-express.set('view engine', 'ejs');
-express.set('views', './app/views'); //define o caminho
-
-load('routes', {
-        cwd: 'app' //pasta que o load deve procurar
-    })
-    .then('infra')
-    .into(express);
 
 module.exports = function() {
+    //Ativando a engine EJS
+    express.set('view engine', 'ejs');
+    express.set('views', './app/views'); //define o caminho
+
+    //Ativando o BodyParser(Middleware)
+    express.use(bodyParser.urlencoded({
+        extended: true //Permite enviar um form complexo
+    })); //urlencoded é o formato que os dados são enviados por default
+
+    //Carregando e exportando os modulos
+    load('routes', {
+            cwd: 'app' //pasta que o load deve procurar
+        })
+        .then('infra')
+        .into(express);
     return express;
 };
