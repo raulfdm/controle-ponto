7337d3f41b002debd0840324f4063909f38530f9
Raul F. de Melo <raulfdm@hotmail.com>
2016-07-19 10:59:45 -0300

    adicionada validação de erro ao carregar lista de pontos

--- app/routes/ponto.js
+++ app/routes/ponto.js
@@ -1,29 +1,33 @@
-module.exports = function(app) {
-    app.get('/ponto', function(req, res) {
+module.exports = function (app) {
+    app.get('/ponto', function (req, res) {
         var connection = app.infra.connectionFactory();
         var pontoDAO = new app.infra.pontoDAO(connection); //o new cria um novo contexto
 
-        pontoDAO.lista(function(erro, resultados) {
+        pontoDAO.lista(function (erro, resultados) {
 
-            res.render('inicial', {
-                lista: resultados
-            });
+            if (!erro) {
+                res.render('inicial', {
+                    lista: resultados
+                });
+            }else{
+                res.send('p13: Erro ao consultar os dados');
+            }
         });
         connection.end();
     });
 
-    app.get('/ponto/new', function(req, res) {
+    app.get('/ponto/new', function (req, res) {
 
         res.render('cadastra-ponto');
     });
 
-    app.post('/ponto', function(req, res) {
+    app.post('/ponto', function (req, res) {
         var ponto = req.body; //pega corpo da requisição
 
         var connection = app.infra.connectionFactory();
         var pontoDAO = new app.infra.pontoDAO(connection); //o new cria um novo contexto
 
-        pontoDAO.insere(ponto, function(erro, resultado) {
+        pontoDAO.insere(ponto, function (erro, resultado) {
             if (!erro) {
                 res.redirect('/ponto'); //always redirect after post!
             } else {
