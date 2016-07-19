3dd36c7c061f94881f349f7f36e83d0e95d641eb
Raul F. de Melo <raulfdm@hotmail.com>
2016-07-19 13:02:03 -0300

    Adição do Express Validator

--- config/express.js
+++ config/express.js
@@ -1,7 +1,7 @@
 var express = require('express')();
 var load = require('express-load');
 var bodyParser = require('body-parser');
-
+var expressValidator = require('express-validator');
 
 module.exports = function() {
     //Ativando a engine EJS
@@ -12,6 +12,8 @@ module.exports = function() {
     express.use(bodyParser.urlencoded({
         extended: true //Permite enviar um form complexo
     })); //urlencoded é o formato que os dados são enviados por default
+    express.use(bodyParser.json()); //Ativa o middleware de leitura de Json, caso o cliente o mande
+    express.use(expressValidator()); //Ativa o middleware de validação
 
     //Carregando e exportando os modulos
     load('routes', {
