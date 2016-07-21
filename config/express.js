b07932d4a29b7adb66c91dbbcbcfacaacaf868b6
Raul F. de Melo <raulfdm@hotmail.com>
2016-07-21 13:25:44 -0300

    modificado a chamada do express e adicionado o middleware estático

--- config/express.js
+++ config/express.js
@@ -1,25 +1,29 @@
-var express = require('express')();
+var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 
 module.exports = function() {
-    //Ativando a engine EJS
-    express.set('view engine', 'ejs');
-    express.set('views', './app/views'); //define o caminho
+    var app = express();
 
+    //Ativando a engine EJS
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views'); //define o caminho
+
+    //Ativando o Middleware dos arquivos estáticos (CSS,JS,ETC)
+    app.use(express.static('./app/public'));
     //Ativando o BodyParser(Middleware)
-    express.use(bodyParser.urlencoded({
+    app.use(bodyParser.urlencoded({
         extended: true //Permite enviar um form complexo
     })); //urlencoded é o formato que os dados são enviados por default
-    express.use(bodyParser.json()); //Ativa o middleware de leitura de Json, caso o cliente o mande
-    express.use(expressValidator()); //Ativa o middleware de validação
+    app.use(bodyParser.json()); //Ativa o middleware de leitura de Json, caso o cliente o mande
+    app.use(expressValidator()); //Ativa o middleware de validação
 
     //Carregando e exportando os modulos
     load('routes', {
             cwd: 'app' //pasta que o load deve procurar
         })
         .then('infra')
-        .into(express);
-    return express;
+        .into(app);
+    return app;
 };
