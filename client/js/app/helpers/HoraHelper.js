446de50bb3a4251633d066b81db526cd92800089
Raul F. de Melo <melo.raulf@gmail.com>
2016-07-25 23:33:21 -0300

    Refatorada a classe Helper de horas

---
+++ client/js/app/helpers/HoraHelper.js
@@ -0,0 +1,41 @@
+class HoraHelper {
+
+    constructor() {
+        throw new Error('Essa classe não pode ser instanciada');
+    }
+
+
+    static getMilissegundos(hora) {
+        if (!hora) {
+            return 0;
+        }
+        let hour = parseInt(hora.substring(0, 2));
+        let minute = parseInt(hora.substring(3, 5));
+
+        hour = ((hour * 60 /*minuto*/ ) * 60 /*Segundo*/ ) * 1000 /*milissegundo*/ ;
+        minute = ((minute) * 60 /*Segundo*/ ) * 1000 /*milissegundo*/ ;
+
+        return (hour + minute);
+    }
+
+    static getHoraString(milissegundos) {
+        if (!milissegundos) {
+            throw new Error("Milissegundos não pode ser vazio");
+        }
+        let total = ((milissegundos / 1000 /*milissegundo*/ ) / 60 /*minuto*/ / 60 /*segundos*/ );
+
+        var hour = Math.trunc(total);
+        var minute = Math.trunc(((total - hour) * 60));
+
+        if (hour.toString().length == 1) {
+            hour = "0" + hour;
+        }
+
+        if (minute.toString().length == 1) {
+            minute = "0" + minute;
+        }
+        return hour + ":" + minute;
+    }
+
+
+}
