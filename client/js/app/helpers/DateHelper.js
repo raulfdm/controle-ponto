81f64ab43459badb01cd4f79fc4999dca8d64ab2
Raul F. de Melo <melo.raulf@gmail.com>
2016-07-25 23:32:48 -0300

    Adicionada class Helper de Data

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,28 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('Essa classe não pode ser instanciada');
+    }
+
+    static textoParaData(texto) {
+
+        return new Date(texto);
+    }
+
+    static dataParaTexto(data) {
+        let dia = data.getDate();
+        let mes = data.getMonth() + 1;
+        let ano = data.getFullYear();
+
+        if (dia.toString().length == 1) {
+            dia = "0" + dia;
+        }
+        if (mes.toString().length == 1) {
+            mes = "0" + mes;
+        }
+
+        return `${dia}/${mes}/${ano}`;
+
+    }
+
+}
