9dad1a16aceee05688644469e6b4cc241dcad43d
Raul F. de Melo <melo.raulf@gmail.com>
2016-08-13 23:49:03 -0300

    Correção do método dataParaTexto

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,25 +5,35 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
-        
+
         return new Date(texto);
     }
 
     static dataParaTexto(data) {
-        let dia = data.getDate();
-        let mes = data.getMonth() + 1;
-        let ano = data.getFullYear();
 
-        if (dia.toString().length == 1) {
-            dia = "0" + dia;
+        data = new Date(data);
+        try {
+            let dia = data.getDate();
+            let mes = data.getMonth() + 1;
+            let ano = data.getFullYear();
+
+            if (dia.toString().length == 1) {
+                dia = "0" + dia;
+            }
+            if (mes.toString().length == 1) {
+                mes = "0" + mes;
+            }
+
+            //return `${dia}/${mes}/${ano}`;
+            return `${ano}-${mes}-${dia}`;
+        } catch (e) {
+            
+            console.log(e);
+        
+            throw new Error("Erro ao adicionar o ponto na lista!");
+            
+            
         }
-        if (mes.toString().length == 1) {
-            mes = "0" + mes;
-        }
-
-        //return `${dia}/${mes}/${ano}`;
-        return `${ano}-${mes}-${dia}`;
-
     }
 
 }
