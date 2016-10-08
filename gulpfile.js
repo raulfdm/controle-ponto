38d5540c5a60edc0fef8c87c32105dbd317d5c6b
Raul F. de Melo <melo.raulf@gmail.com>
2016-10-08 19:10:38 -0300

    Correção do listener do babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 const gulp = require('gulp'),
-browser = require('browser-sync'),
-babel = require('gulp-babel')
-sourcemaps = require('gulp-sourcemaps');
+    browser = require('browser-sync'),
+    babel = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('babel',function(){
+gulp.task('babel', function () {
+
     gulp.src('src/js/app-es6/**/*')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('src/js/app/'))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/js/app/'))
 })
 
-gulp.task('server',function(){
+gulp.task('server', function () {
     browser.init({
-        server:{
+        server: {
             baseDir: 'src/'
         }
     })
 
-    gulp.watch('src/**/*').on('change',browser.reload);
-    gulp.watch('src/js/app-es6**/*').on('change',babel);
-
+    //Change Listeners 
+    gulp.watch('src/js/app-es6/**/*.js', ['babel']);
+    gulp.watch('src/**/*').on('change', browser.reload);
 })
