8a98acbc48cdddbf39d48f4e7cb980a7ebbffc5d
Raul F. de Melo <melo.raulf@gmail.com>
2016-10-08 18:16:11 -0300

    Adicionado o Babel para transpilar o código ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require('gulp'),
-browser = require('browser-sync');
+browser = require('browser-sync'),
+babel = require('gulp-babel')
+sourcemaps = require('gulp-sourcemaps');
 
+gulp.task('babel',function(){
+    gulp.src('src/js/app-es6/**/*')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('src/js/app/'))
+})
 
 gulp.task('server',function(){
     browser.init({
@@ -9,5 +20,7 @@ gulp.task('server',function(){
         }
     })
 
-    gulp.watch('src/**/*').on('change',browser.reload)
+    gulp.watch('src/**/*').on('change',browser.reload);
+    gulp.watch('src/js/app-es6**/*').on('change',babel);
+
 })
