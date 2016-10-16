af7cecef5ad5ebc0db5faeb60b33e2e8722c619c
Raul F. de Melo <melo.raulf@gmail.com>
2016-10-15 23:17:03 -0300

    Fix issue #6 - Implementação de autenticação - Corrigido texto da tela; -  Adicionado ID nos botões; -  Adicionado a validação do usuário logado; -  Adicionado evento de Dropdown no botão do menu; - Adicionado evento de cadastro e autenticação;

--- src/js/app-es6/boot.js
+++ src/js/app-es6/boot.js
@@ -1,21 +1,31 @@
 import 'js/lib/fire-base.js';
 import './polyfill/fetch-api';
 import { currentInstance } from './controllers/PontoController';
-import MaskHelper from './helpers/MaskHelper'
+import MaskHelper from './helpers/MaskHelper';
+//
+firebase.auth().onAuthStateChanged(function(user) {    
+    if (user) {        
+        let pontoController = new currentInstance();
+
+        document.querySelector('.email-usuario').innerText = user.email;        
+        document.querySelector('.form-cad-ponto').onsubmit = pontoController.adicionaPonto.bind(pontoController);
+        document.querySelector('.btn-carregar').onclick = pontoController.importaPontos.bind(pontoController);
+        document.querySelector('.btn-exclui').onclick = pontoController.limpaGrid.bind(pontoController);
+        document.querySelectorAll('.input-hora').forEach((campo) => {
+            campo.onkeypress = MaskHelper.mask.bind(pontoController);
+            //campo.oninvalid = campo.setCustomValidity('Por favor, preencha o campo');    
+        });
 
 
-let pontoController = new currentInstance();
+        //Init
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 16 // Creates a dropdown of 15 years to control year            
 
-document.querySelector('.form-cad-ponto').onsubmit = pontoController.adicionaPonto.bind(pontoController);
-document.querySelector('.btn-carregar').onclick = pontoController.importaPontos.bind(pontoController);
-document.querySelector('.btn-exclui').onclick = pontoController.limpaGrid.bind(pontoController);
-document.querySelectorAll('.input-hora').forEach((campo) => {
-    campo.onkeypress = MaskHelper.mask.bind(pontoController);
-    //campo.oninvalid = campo.setCustomValidity('Por favor, preencha o campo');    
-});
-
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 16 // Creates a dropdown of 15 years to control year            
-
+        });
+        $('.dropdown-button').dropdown();
+        
+    } else {
+        window.location.href = "/autenticar.html";        
+    }
 });
