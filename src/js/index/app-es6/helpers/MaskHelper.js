9d4bf11049f31c054bd47ef6ccc3d86e7df3bf08
Raul Felipe de Melo <melo.raulf@gmail.com>
2016-10-26 13:37:42 -0200

    Fix issue #19 - Validações e Correções - Corrigido o submit do form; -  Definido altura mínima para o modal; -  Corrigido o método de aplicação da máscara.    + Foi observado que no mobile, os eventos não se comportam da mesma maneira. Assim, foi adicionado uma validação onde verifica se o keyCode é 229(sempre será no teclado mobile), e caso  seja, o valor a ser considerado será sempre o último digitado;     - Adaptações na lógica para trabalhar com o KeyUp

--- src/js/index/app-es6/helpers/MaskHelper.js
+++ src/js/index/app-es6/helpers/MaskHelper.js
@@ -6,26 +6,24 @@ class MaskHelper {
         //quando usamos THIS dentro do método, ele se refere ao contexto PontoController
         let self = this;
 
-        self._eventoDoElemento;        
-        self._elemento;        
+        self._eventoDoElemento;
+        self._elemento;
     }
 
     static mask(evento) {
-        self._eventoDoElemento = evento;        
-        self._elemento = evento.target;         
+        self._eventoDoElemento = evento;
+        self._elemento = evento.target;
         self._elemento.value = MaskHelper._hora24(self._elemento.value);
     };
 
 
     static _hora24(valor) {
-        
-        let chave = self._eventoDoElemento.key;
 
+        let chave = self._eventoDoElemento.keyCode == 229 ? valor.slice(-1) : self._eventoDoElemento.key;
         //Se já estiver preenchido com a hora certa
-        //Não insere e retorna o que está no campo        
-        if (valor.length == 5) {
-            self._eventoDoElemento.preventDefault();
-            return valor;
+        //Não insere e retorna o que está no campo
+        if (valor.length > 5) {
+            return valor.slice(0, -1);
         }
 
         //Valida se tem caracteres diferentes de Números
@@ -33,7 +31,7 @@ class MaskHelper {
 
         //Valida se é o primeiro elemento e
         //se o que está sendo passado, é um número válido
-        if (valor.length == 0 && !/[0-2]/.test(chave)) {
+        if (valor.length == 1 && !/[0-2]/.test(chave)) {
             self._eventoDoElemento.preventDefault();
             valor = '';
             return valor;
@@ -44,24 +42,22 @@ class MaskHelper {
         //2- O primeiro elemento é o algarismo 2?
         //3- O que está sendo digitado, é um número de 0 a 3?
         //Obs.: essa regra foi criada para evitar horas inválidas (24,25,29, por exemplo);
-        if (valor.length == 1 && /([2])/.test(valor.substring(0, 1)) && !/([0-3])/.test(chave)) {
-            self._eventoDoElemento.preventDefault();
+        if (valor.length == 2 && /([2])/.test(valor.substring(0, 1)) && !/([0-3])/.test(chave)) {
+            return valor.slice(0, -1);
         }
 
         //Valida se o terceiro digito é de 0 a 5
-        if (valor.length == 2 && !/([0-5])/.test(chave)) {
-            self._eventoDoElemento.preventDefault();
+        if (valor.length == 3 && !/([0-5])/.test(chave)) {
+            return valor.slice(0, -1);
         }
 
         //Valida se está no último digito
-        if (valor.length == 3) {
+        if (valor.length == 4) {
             //Cancela o evento, para não duplicar o valor a ser enviado no campo
             self._eventoDoElemento.preventDefault();
-
+            //debugger;
             //Se o valor a ser enviado é um número
             if (/(\d)/.test(chave)) {
-                //concatena o valor do campo com o valor enviado
-                valor += chave;
                 //insere a mascara de hora
                 valor = valor.replace(/([0-2]\d)([0-5]\d)/g, "$1:$2");
             }
