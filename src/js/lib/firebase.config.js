92c823682568729dcc156aebf2852b9e9b84ee8f
Raul Felipe de Melo <melo.raulf@gmail.com>
2016-10-31 15:43:48 -0200

    Fix issue #46 - Remoção do código Antigo

--- src/js/lib/firebase.config.js
+++
@@ -1,8 +0,0 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCg5W72AyCxzbLkYhHU-P4nDT6hnllUbhM",
-    authDomain: "controle-ponto-cc043.firebaseapp.com",
-    databaseURL: "https://controle-ponto-cc043.firebaseio.com",
-    storageBucket: "",
-  };
-  firebase.initializeApp(config);
